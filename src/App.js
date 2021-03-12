6c37e6487f5475e622604a0b580d360772cb94a7
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-03-13 00:10:47 +0530

    added code to make API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends React.Component {
+	state = {
+		advice: "",
+	};
+
+	// lifecycle method to make API request which gets called when
+	// the current component gets mounted
+	componentDidMount() {
+		this.fetchAdvice();
+	}
+
+	// we have to call the api twice, once during loading
+	// and another after key press hence we create a method
+	fetchAdvice = async () => {
+		const apiData = await axios.get("https://api.adviceslip.com/advice");
+		console.log(apiData.data.slip.advice);
+	};
+
 	render() {
 		return (
 			<React.Fragment>
