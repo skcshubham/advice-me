d26ff73af0df267442dfa42057550a18a0e95012
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-03-13 14:47:26 +0530

    adding button and navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./Navbar";
 import axios from "axios";
 import "./App.css";
 
@@ -15,7 +16,7 @@ class App extends React.Component {
 
 	// we have to call the api twice, once during loading
 	// and another after key press hence we create a method
-	fetchAdvice = async () => {
+	fetchAdvice = () => {
 		axios
 			.get("https://api.adviceslip.com/advice")
 			.then((response) => {
@@ -23,20 +24,32 @@ class App extends React.Component {
 				this.setState({
 					advice: advice,
 				});
-				console.log(advice);
 			})
 			.catch((error) => {
-				console.log("Error", error);
+				const advice = "Either your Internet is off or the API server is down!";
+				this.setState({
+					advice: advice,
+				});
 			});
 	};
 
 	render() {
 		return (
-			<div className="App">
-				<div className="App-card">
-					<h1 className="App-heading">{this.state.advice}</h1>
+			<React.Fragment>
+				<Navbar />
+				<div className="App">
+					<div className="App-card">
+						<h1 className="App-heading pb-1">{this.state.advice}</h1>
+						<button
+							type="button"
+							className="button btn btn-md btn-outline-info mb-2"
+							onClick={this.fetchAdvice}
+						>
+							<span>Give me advice!</span>
+						</button>
+					</div>
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Navbar extends React.Component {
+	render() {
+		return (
+			<nav className="navbar navbar-light bg-light">
+				<div className="container">
+					<a
+						className="navbar-brand"
+						href="https://github.com/skcshubham/advice-me"
+						target="_blank"
+						rel="noreferrer"
+						style={{ fontFamily: "monospace", fontSize: "1.6rem" }}
+					>
+						Advice Me!
+					</a>
+					<div className="d-flex">
+						<p className="m-0">built with Advice API</p>
+						<a
+							href="https://api.adviceslip.com/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<i className="fas fa-robot fa-lg pl-1 m-1"></i>
+						</a>
+					</div>
+				</div>
+			</nav>
+		);
+	}
+}
+
+export default Navbar;
